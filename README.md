5ca7c0d5b1d11508a23720c9716db20b2bf81e45
Александр Ромашков <eho231@mail.ru>
2023-02-01 21:44:22 +0300

    Before_first_review

--- README.md
+++ README.md
@@ -1,1 +1,138 @@
-# praktikum_new_diplom
+# Foodgram - Продуктовый помощник
+
+![example workflow](https://github.com/Wests007/foodgram-project-react/actions/workflows/foodgram_workflow.yml/badge.svg)  
+
+## Стек технологий
+
+Python 3,
+Django 2.2,
+Django REST Framework,
+PostgreSQL,
+Nginx,
+Gunicorn,
+Docker,
+GitHub Actions,
+Yandec.Cloud.
+
+## Описание проекта
+
+Foodgram - лучший сервис для публикации рецептов.  
+Пользователи могут создавать свои рецепты, читать рецепты других пользователей, подписываться на интересных авторов, добавлять лучшие рецепты в избранное, а также создавать список покупок и скачивать его в pdf формате.
+
+## Установка проекта локально
+
+* Клонировать репозиторий и перейти в него в командной строке:
+```bash
+git clone git@github.com:Wests007/foodgram-project-react.git
+```
+
+```bash
+cd foodgram-project-react
+```
+
+* Cоздать и активировать виртуальное окружение:
+
+```bash
+python -m venv venv
+```
+
+```bash
+source venv/scripts/activate
+```
+
+* Cоздать файл `.env` в директории `/infra/` с содержанием:
+
+```
+SECRET_KEY=секретный ключ django
+DB_ENGINE=django.db.backends.postgresql
+DB_NAME=postgres
+POSTGRES_USER=postgres
+POSTGRES_PASSWORD=postgres
+DB_HOST=db
+DB_PORT=5432
+```
+
+* Перейти в директирию backend, обновить менеджер пакетов и установить зависимости из файла requirements.txt:
+
+```bash
+cd backend/
+```
+
+```bash
+python -m pip install --upgrade pip
+```
+
+```bash
+pip install -r requirements.txt
+```
+
+* Выполнить миграции:
+
+```bash
+python manage.py migrate
+```
+
+* Загрузить ингридиенты и теги:
+
+```bash
+python manage.py load_ingrs
+```
+
+```bash
+python manage.py load_tags
+```
+
+* Собрать статику:
+
+```bash
+python manage.py collectstatic --noinput
+```
+
+* Запустите сервер:
+
+```bash
+python manage.py runserver
+```
+
+## Запуск проекта в Docker-контейнере
+
+Параметры запуска описаны в файлах `docker-compose.yml` и `nginx.conf` которые находятся в директории `infra/`.  
+При необходимости добавьте/измените адреса проекта в файле `nginx.conf`
+
+* Запустить docker-compose:
+```bash
+docker-compose up -d --build
+```  
+  > После сборки появляются 3 контейнера:
+  > 1. контейнер базы данных **db**
+  > 2. контейнер приложения **backend**
+  > 3. контейнер web-сервера **nginx**
+* Применить миграции:
+```bash
+docker-compose exec backend python manage.py migrate
+```
+* Загрузить ингредиенты:
+```bash
+docker-compose exec backend python manage.py load_ingrs
+```
+* Загрузить теги:
+```bash
+docker-compose exec backend python manage.py load_tags
+```
+* Создать суперпользователя:
+```bash
+docker-compose exec backend python manage.py createsuperuser
+```
+* Собрать статику:
+```bash
+docker-compose exec backend python manage.py collectstatic --noinput
+```
+## Сайт
+Сайт доступен по ссылке:
+
+## Документация к API
+API документация доступна по ссылке (создана с помощью redoc):
+
+## Авторы
+[Ромашков Александр](https://github.com/Wests007) - Разработал бэкенд и деплой сервиса.
+[Яндекс.Практикум](https://github.com/yandex-praktikum) - Фронтенд для сервиса.
---
+++ backend/backend/settings.py
@@ -0,0 +1,161 @@
+import os
+from pathlib import Path
+
+from dotenv import load_dotenv
+
+load_dotenv(dotenv_path=Path('../infra') / '.env')
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+SECRET_KEY = os.getenv(
+    'SECRET_KEY',
+    default='(we6fkv5)!*@=p8zecz4a!t9tt=3(5s#bb1p&pup(79e=ck+=v'
+)
+
+DEBUG = True
+
+ALLOWED_HOSTS = ['*']
+
+CSRF_TRUSTED_ORIGINS = [
+    'http://*localhost',
+    'https://*localhost',
+    'http://51.250.86.177',
+    'https://51.250.86.177',
+]
+
+INSTALLED_APPS = [
+    'api.apps.ApiConfig',
+    'users.apps.UsersConfig',
+    'recipes.apps.RecipesConfig',
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'django_filters',
+    'djoser',
+    'corsheaders',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
+]
+
+ROOT_URLCONF = 'backend.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'backend.wsgi.application'
+
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_URLS_REGEX = r'^/api/.*$'
+
+DATABASES = {
+    'default': {
+        'ENGINE': os.getenv(
+            'DB_ENGINE',
+            default='django.db.backends.postgresql'
+        ),
+        'NAME': os.getenv('DB_NAME', default='postgres'),
+        'USER': os.getenv('POSTGRES_USER', default='postgres'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='postgres'),
+        'HOST': os.getenv('DB_HOST', default='db'),
+        'PORT': os.getenv('DB_PORT', default='5432')
+    }
+}
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+LANGUAGE_CODE = 'ru-RU'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = False
+
+STATIC_URL = '/static/'
+
+STATIC_URL = '/backend_static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'backend_static')
+
+MEDIA_URL = '/backend_media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'backend_media')
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+    ],
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.UserRateThrottle',
+        'rest_framework.throttling.AnonRateThrottle',
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'user': '10000/day',
+        'anon': '100/day',
+    },
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 6
+}
+
+DJOSER = {
+    "LOGIN_FIELD": 'email',
+    "SEND_ACTIVATION_EMAIL": False,
+    'HIDE_USERS': False,
+    "SERIALIZERS": {
+        "user_create": "users.serializers.CustomUserCreateSerializer",
+        "user": "users.serializers.CustomUserSerializer",
+        "current_user": "users.serializers.CustomUserSerializer",
+    },
+    'PERMISSIONS': {
+        'user': ['djoser.permissions.CurrentUserOrAdminOrReadOnly'],
+        'user_list': ['rest_framework.permissions.AllowAny']
+    },
+}
+
+AUTH_USER_MODEL = 'users.User'
