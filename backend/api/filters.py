d8687c036aa96dfe4a9475839c77770823ae05c4
Александр Ромашков <eho231@mail.ru>
2023-02-06 00:16:29 +0300

    After_first_review

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -10,6 +10,7 @@ class IngredientSearchFilter(SearchFilter):
 
 class RecipeFilter(FilterSet):
     tags = filters.AllValuesMultipleFilter(
+        conjoined=True,
         field_name='tags__slug',
         lookup_expr='in'
     )
